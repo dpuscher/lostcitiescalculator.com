ee2abca86bb2009048707a60f892bee7c5c5631e
Daniel Puscher <dev@daniel-puscher.de>
2025-01-09 21:44:19 +0100

    feat: integrate Serwist for service worker support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-};
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
+const nextConfig: NextConfig = withSerwist({
+  output: "export",
+});
 
 export default nextConfig;
